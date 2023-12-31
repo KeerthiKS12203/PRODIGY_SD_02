f7e3ea31046c8425ba38789e79b15a91b00b5b16
KeerthiKS12203 <135096446+KeerthiKS12203@users.noreply.github.com>
2023-12-31 18:40:33 +0530

    Update RandNumber.java

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,85 +1,102 @@
-package coder;
-
-import javax.swing.*;
-import java.awt.*;
+package pkg1;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
 
-public class GuessTheNumber extends JFrame {
+public class RandNum implements ActionListener {
+	JFrame f;
+	JLabel l1,l2,l3;
+	JTextField t1;
+	JButton b1,b2;
+	int num=(int)(Math.random()*(100-0)+1);
+	int trial=0;
+	
+	RandNum(){
+		f=new JFrame();
+		
+		l1=new JLabel("Guess the number in my mind !");
+		l1.setBounds(60,30,200,20);
+		
+		l2=new JLabel("Hint: It lies between 0 and 50");
+		l2.setBounds(50,70,200,20);
+		
+		l3=new JLabel();
+		l3.setBounds(50,110,500,60);
+		l3.setVisible(false);
+		
+		t1=new JTextField();
+		t1.setBounds(120,210,60,40);
+		
+		b1=new JButton("Enter");
+		b1.setBounds(110,270,80,20);
+		
+		b2=new JButton("No");
+		b2.setBounds(110,270,80,20);
+		b2.setVisible(false);
+		
+		b1.addActionListener(this);
+		
+		b2.addActionListener(this);
+		
+		f.add(b1);
+		f.add(l1);
+		f.add(l2);
+		f.add(t1);
+		f.add(l3);
+		f.add(b2);
+		
+		f.setTitle("Number Game");
+		f.setSize(550,340);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+		
+	public void actionPerformed(ActionEvent e) {
+		int guess=Integer.parseInt(t1.getText());
+		l3.setVisible(true);
+		
+		if(guess==num) {
+			l2.setText("<html>Hurray! You guessed it right.<html>");
+			l3.setText("<html>You have taken "+trial+" chances to <br>guess the correct number.<br>Do you want to play again?<html>");
+			trial=0;
+			b1.setText("Yes");
+			b1.setBounds(75,270,70,20);
+			b2.setText("No");
+			b2.setBounds(170,270,70,20);
+			b2.setVisible(true);
+			b2.addActionListener(this);
+			if(e.getSource()==b2) {
+				f.dispose();
+			}
+			else if(e.getSource()==b1) {
+				int num=(int)(Math.random()*(100-0)+1);
+				int trial=0;
+				l2.setText("Hint: It lies between 0 and 100");
+				l2.setBounds(50,70,300,20);
+				l3.setVisible(false);
+				b1.setText("Enter");
+				b1.setBounds(110,270,80,20);
+				b2.setVisible(false);
 
-    private int secretNumber;
-    private int attempts;
 
-    private JTextField guessField;
-    private JLabel feedbackLabel;
+			}
+			
+			
+		}
+		else if(num<guess) {
+			l3.setText("The target is lesser than your guess. Try a smaller number");
+			++trial;
+		}else {
+			l3.setText("The target is greater than your guess. Try a larger number");
+			++trial;
+		}
+	}
 
-    public GuessTheNumber() {
-        super("Guess the Number Game");
-        secretNumber = generateRandomNumber();
-        attempts = 0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new RandNum();
+	}
 
-        setupUI();
-    }
+	
 
-    private void setupUI() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 150);
-        setLocationRelativeTo(null);
-        setLayout(new BorderLayout());
-
-        JLabel instructionLabel = new JLabel("Enter your guess (between 1 and 100):");
-        guessField = new JTextField(10);
-        JButton guessButton = new JButton("Guess");
-        feedbackLabel = new JLabel("");
-
-        guessButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                checkGuess();
-            }
-        });
-
-        JPanel inputPanel = new JPanel();
-        inputPanel.add(instructionLabel);
-        inputPanel.add(guessField);
-        inputPanel.add(guessButton);
-
-        add(inputPanel, BorderLayout.NORTH);
-        add(feedbackLabel, BorderLayout.CENTER);
-    }
-
-    private int generateRandomNumber() {
-        return (int) (Math.random() * 100) + 1;
-    }
-
-    private void checkGuess() {
-        try {
-            int userGuess = Integer.parseInt(guessField.getText());
-            attempts++;
-
-            if (userGuess == secretNumber) {
-                displayWinnerMessage();
-            } else if (userGuess < secretNumber) {
-                feedbackLabel.setText("Too low! Try again.");
-            } else {
-                feedbackLabel.setText("Too high! Try again.");
-            }
-        } catch (NumberFormatException ex) {
-            feedbackLabel.setText("Invalid input. Please enter a number.");
-        }
-    }
-
-    private void displayWinnerMessage() {
-        feedbackLabel.setText(String.format("Congratulations! You guessed the number %d in %d attempts.", secretNumber, attempts));
-        guessField.setEnabled(false);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new GuessTheNumber().setVisible(true);
-            }
-        });
-    }
 }
